2a2083ee336b35c9b5ea95118b8ef587130d9785
Kristina Spurgin <kspurgin@email.unc.edu>
2014-07-15 16:13:17 -0400

    new and deleted files

---
+++ afro_americana_796_extract.rb
@@ -0,0 +1,74 @@
+# ruby 1.9
+# runs on .mrc file
+
+require "marc"
+
+exit if Object.const_defined?(:Ocra)
+
+# Set files
+mrcfile = 'data/796input.mrc'
+outfile = 'output/796data.txt'
+
+# Read in MARC input files
+@recs = []
+MARC::Reader.new(mrcfile).each {|rec| @recs << rec}
+
+# Set up format for output
+@lines = []
+@lines << ['001', 'whole field', 'flag', 'e', 'needs review']
+
+@recs.each do |rec|
+  # Get 001 value for each record
+  if rec['001'].value
+    v001 = rec['001'].value
+  else
+    v001 = ""
+  end
+
+  # Look at each 796 field
+  rec.each_by_tag('796') do |the796|
+    # Write whole field to a string, for reference
+    vs796 = the796.to_s
+    # Set/empty counters
+    ct9 = 0
+    cte = 0
+    # Create array to hold any $e values
+    alle = []
+    # Then, look at each subfield in that 796...
+    the796.each do |sf|
+      # count $9s
+      ct9 += 1 if sf.code == '9'
+      if sf.code == 'e'
+        # count $es
+        cte += 1
+        # Send normalized (spaces, commas, periods removed) $e values to array
+        alle << sf.value.gsub(/\s*[.,]\s*$/, '')
+      end
+    end
+    # Set flags based on subfield counts
+    flag = ""
+    flag = "No $9 AND No $e" if ct9 == 0 && cte == 0
+    flag = "Has $9, No $e" if ct9 > 0 && cte == 0
+    flag = "More than one $e - split into separate fields" if cte > 1
+
+    # If there are no $es, send one line per 796 to output list
+    @lines << [v001, vs796, flag, "", ""] if cte == 0
+    # If there is 1 or more $e, send one line per $e to output list
+    if cte > 0
+      alle.each do |e|
+        @lines << [v001, vs796, flag, e, ""]
+      end
+    end
+  end
+end
+
+# Write output list to output file, tab delimited
+File.open outfile, "wb" do |out|
+  @lines.each do |line|
+    puts line.inspect
+    line_string = line.join("\t")
+    out.puts line_string
+  end
+end
+
+exit
