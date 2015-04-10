ef3b91856781d47b608fb2db9be87f4848bf6b0c
Kristina Spurgin <kspurgin@email.unc.edu>
2015-04-10 16:34:23 -0400

    new script added

--- extract_by_field_value.rb
+++ extract_by_field_value.rb
@@ -1,7 +1,7 @@
 # ruby 1.9
 # runs on .mrc file
 # usage:
-# ruby extract_by_field_value.rb path/to/mrcfile path/to/values 001
+# ruby extract_by_field_value.rb
 
 require "trie"
 require "marc"
@@ -12,9 +12,36 @@ valfile = "data/value_list.txt"
 tag = ask("Enter the 3-digit MARC field tag you want to match on (example: 001)")
 subfield = ask("If you want to match on a specific subfield in that MARC tag, enter the subfield delimter (example: z). If you want to match on the whole field, just hit Enter.")
 
+if subfield == ""
+    subfield = nil
+end
+
+write_matches = ask("Do you want to create a file of MARC records that match the values in your list? Type y or n and hit Enter.")
+write_non_matches = ask("Do you want to create a file of MARC records that DO NOT match the values in your list? Type y or n and hit Enter.")
+
 puts "Reading in MARC records...\n"
-@recs = []
-MARC::Reader.new(mrcfile).each {|rec| @recs << rec}
+@rec_info = Hash.new
+
+if subfield
+  matchpoint = "#{tag}$#{subfield}"
+else
+  matchpoint = "#{tag}"
+end
+
+MARC::Reader.new(mrcfile).each do |rec|
+  rec_id = rec['001'].value
+  if subfield
+    match_data = rec[tag][subfield].value
+  else
+    match_data = rec[tag].value
+  end
+
+  if @rec_info.has_key?(match_data)
+    @rec_info[match_data] << rec_id
+  else
+    @rec_info[match_data] = [rec_id]
+  end
+end
 
 puts "Reading in values to match on...\n"
 nums = File.open(valfile, "r").readlines
@@ -25,41 +52,41 @@ nums.each do |num|
   oak.insert(num.chomp, 1)
 end
 
-if subfield
-  matchpoint = "#{tag}$#{subfield}"
-else
-  matchpoint = "#{tag}"
-end
-
 puts "Checking for matches on #{matchpoint}...\n"
 
-@match = []
-@nomatch = []
+@match_ids = []
+@nomatch_ids = []
 
-@recs.each do |rec|
-  if subfield == ""
-    subfield = nil
-  end
-  val = rec[tag].value unless subfield
-  val = rec[tag][subfield] if subfield
-  #puts val
-  lookup = oak.find(val).values
-  #p lookup
+puts @rec_info.size
+
+@rec_info.each_key do |k|
+  lookup = oak.find(k).values
   if lookup.size > 0
-    @match << rec
+    @match_ids << @rec_info[k]
   else
-    @nomatch << rec
+    @nomatch_ids << @rec_info[k]
   end
 end
 
-puts "Writing out matching MARC records...\n"
-  matchwriter = MARC::Writer.new("data/match_on_#{tag}#{subfield}.mrc")
-  @match.each {|rec| matchwriter.write(rec)}
-  matchwriter.close
 
-puts "Writing out non-matching MARC records...\n"
+puts "Writing out MARC records...\n"
+@match_ids.flatten!.uniq!
+@nomatch_ids.flatten!.uniq!
+matchwriter = MARC::Writer.new("data/match_on_#{tag}#{subfield}.mrc")
 nomatchwriter = MARC::Writer.new("data/no_match_on_#{tag}#{subfield}.mrc")
-  @nomatch.each {|rec| nomatchwriter.write(rec)}
-  nomatchwriter.close
+
+MARC::Reader.new(mrcfile).each do |rec|
+  thisid = rec['001'].value
+  if write_matches == 'y'
+    matchwriter.write(rec) if @match_ids.include?(thisid)
+  end
+  if write_non_matches == 'y'
+    nomatchwriter.write(rec) if @nomatch_ids.include?(thisid)
+  end
+end
+
+matchwriter.close
+nomatchwriter.close
+
 
 puts "Done!"
