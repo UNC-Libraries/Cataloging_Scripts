95628caf1e9503b0bbf377601a65ff2129c273ea
U-AD\kspurgin <kspurgin@AALTS60.lib.unc.edu>
2016-05-09 18:43:47 -0400

    strip OCLC prefix and slashes before comparing 001s

--- find_a_minus_b.rb
+++ find_a_minus_b.rb
@@ -25,29 +25,36 @@ MARC::Reader.new(b).each do |rec|
   elsif m001s.size == 0
     puts "#{b} contains record with NO 001 field. Results do not include this record!"
   else
-    b001s << m001s[0].value
+    the001 = m001s[0].value.gsub!(/\s*oc?[mn](\d+)[^0-9]*/, '\1')
+    b001s << the001
   end
 end #MARC::Reader.new(b).each do |rec|
 puts "#{b001s.size} 001s in #{b}"
+#b001s.each {|v| puts v}
 
 writer = MARC::Writer.new(outfile)
 
 # Go through records in file a, writing each to outfile unless its 001 was in file b
 puts "Examining records from #{a}..."
+all_ct = 0
 keeper_ct = 0
 MARC::Reader.new(a).each do |rec|
+  all_ct += 1
   m001s = rec.find_all {|field| field.tag == '001'}
   if m001s.size > 1
     puts "#{a} contains record with more than 1 001, including #{m001s[0].value}. Results do not include this record!"
   elsif m001s.size == 0
     puts "#{a} contains record with NO 001 field. Results do not include this record!"
   else
-    unless b001s.include?(m001s[0].value)
+    the001 = m001s[0].value
+    the001.gsub!(/\s*oc?[mn](\d+)[^0-9]*/, '\1')
+ #   puts the001
+    unless b001s.include?(the001)
       writer.write(rec)
       keeper_ct += 1
     end
   end
 end #MARC::Reader.new(a).each do |rec|
-puts "\n\nDONE! #{keeper_ct} records in #{a} and not in #{b}."
+puts "\n\nDONE!\n#{all_ct} records in #{a}.\n#{keeper_ct} records kept.\n"
 
 writer.close
