16bd899865825603a5bc1d233e446e21c869f1d0
kspurgin <kspurgin@email.unc.edu>
2016-07-22 11:46:19 -0400

    still in development...

--- wcm_reason_for_change_processing.rb
+++ wcm_reason_for_change_processing.rb
@@ -18,6 +18,13 @@ unless File.exist?(mrcfile)
   exit
 end
 
+# Prepares to create a log file if there are any warnings/errors
+t = Time.now
+timestring = t.strftime("%Y-%m-%d_%H-%M")
+log = "output/log_#{timestring}.csv"
+errs = [['001 value','Message']]
+
+
 rules = {
   "Collection change" => "load",
   "KB URL change" => "load",
@@ -38,31 +45,53 @@ lti_fields = [
 
 marcwriter = MARC::Writer.new("output/edited.mrc")
 
-def check655(rec)
+def check655(rec, my001)
+  #LTI controls genre/form headings following these patterns:
+  # - 655 .0 with no $2
+  # - 655 .7 with $2lcsh
+  # - 655 .7 with $2lcgft
   findings = ''
-  my001 = rec['001'].value.gsub!(/\D/,'')
+
+  # get 655s from existing records in ILS, by calling the classic web OPAC
+  ex655s = []
+  # URL does e-book scoped OCLC number search
   marcurl = URI("http://webcat.lib.unc.edu/search~S33?/o#{my001}/o#{my001}/1%2C1%2C1%2CB/marc&FF=o#{my001}&1%2C1%2C/indexsort=-")
   response = Net::HTTP.get_response(marcurl)
   if response.message == 'OK'
-    my655s = []
+    response.body.gsub!(/\n       /m, '')
     lines = response.body.split("\n")
     lines.each do |ln|
       if /^655 .. (?!Electronic)/ =~ ln
-        genre = /^655 .. ([^|]*)/.match(ln)
-        my655s << genre[1]
+        if /^655 .0/ =~ ln
+          g = /^655 .. (.*)$/.match(ln) unless ln.include?('$2')
+          ex655s << g[1]
+        elsif /^655 .7 .*(\$2| )(lcsh|lcgft|lcgtf|lgft)/ =~ ln
+          g = /^655 .. (.*)$/.match(ln)
+          ex655s << g[1]
+        end
       end
     end
-    puts my655s.inspect
   else
     findings = 'LTI'
   end
+  puts ex655s.inspect if ex655s.size > 0
 end
 
 reader = MARC::Reader.new(mrcfile)
 for rec in reader
   reasons = []
   chfields = []
+  my001 = rec['001'].value.gsub!(/\D/,'')
   
+  # Check for language of cataloging. If not English (blank or eng), then:
+  #  - report to log
+  #  - do not output record -- skip to next record without further processing
+  catlang = rec['040']['b']
+  unless catlang == 'eng' || catlang == ''
+    errs << [my001, 'Non-English language of cataloging']
+    next
+  end
+
   rec.fields("598").each do |f|
     sfa = f['a']
     if sfa.start_with?("Reason for updated record: ")
@@ -92,10 +121,11 @@ for rec in reader
   end
   reasons.uniq!
 
+
   keeper_ct = 0
   reasons.each do |r|
     if r == "655 only"
-      check655(rec)
+        check655(rec, my001)
     elsif r == "other varfields"
       keeper_ct += 1
     elsif r == "LTI varfields"
